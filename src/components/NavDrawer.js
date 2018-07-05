f6c8b73903badd2e7893bdf4071f6133911eda73
Jizu Sun <jizu.sun@sap.com>
2018-07-06 00:33:54 +0800

    add toggle method on FAB

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -6,23 +6,33 @@ import FloatingActionButton from 'material-ui/FloatingActionButton'
 import Menu from 'material-ui/svg-icons/navigation/menu'
 
 class NavDrawer extends Component {
+	state = {
+		open: false
+	}
+
+	toggle = () => {
+		this.setState((prevState, props) => {
+			return {
+				open: !prevState.open
+			}	
+		})
+	}
 	render() {
 		return (
 			<div>
-				<FloatingActionButton>
+				<FloatingActionButton onClick={this.toggle}>
 					<Menu/>
 				</FloatingActionButton>
 
-				<Drawer>
-				<div
-				  style={{
-				    height: '200px',
-				    width: '100%',
-				    backgroundColor: 'salmon'
-				  }}
-				>
-				  LoginContainer
-				</div>
+				<Drawer open={this.state.open} >
+					<div
+					  style={{
+					    height: '200px',
+					    width: '100%'
+					  }}
+					>
+					  LoginContainer
+					</div>
 					<Divider/>
 					<MenuItem 
 						primaryText={'Play'}
